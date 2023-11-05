33290d6b77c3a48260deac222ea8bd8b61cec904
Malika Sadullaeva <malika.sadullaeva@scandiweb.com>
2023-11-05 14:09:05 +0500

    feat: add search button

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Search from "../seacrh/Search";
 import "./Input.css";
 
 function Input() {
-  return <input type="text" />;
+  return (
+    <>
+      <input type="text" />
+      <Search />
+    </>
+  );
 }
 
 export default Input;
---
+++ src/components/seacrh/Search.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Search.css";
+
+function Search() {
+  return <button type="submit">Search Pokemon</button>;
+}
+
+export default Search;
