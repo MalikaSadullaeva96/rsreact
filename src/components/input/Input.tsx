61ce7108a7557ca862f4ae2110ab6fb4ae004992
Malika Sadullaeva <malika.sadullaeva@scandiweb.com>
2023-11-05 14:50:00 +0500

    feat: add api

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "../seacrh/Search";
 import "./Input.css";
 
 function Input() {
+  const [pokemonName, setPokemonName] = useState("");
   return (
     <>
-      <input type="text" />
-      <Search />
+      <input
+        type="text"
+        onChange={(event) => {
+          setPokemonName(event.target.value);
+        }}
+      />
+      <Search pokemonName={pokemonName} />
     </>
   );
 }
--- src/components/seacrh/Search.tsx
+++ src/components/seacrh/Search.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Search.css";
+import axios from "axios";
 
-function Search() {
-  return <button type="submit">Search Pokemon</button>;
+interface SearchProps {
+  pokemonName: string;
+}
+
+function Search({ pokemonName }: SearchProps) {
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => console.log(response));
+  };
+
+  return (
+    <button onClick={searchPokemon} type="submit">
+      Search Pokemon
+    </button>
+  );
 }
 
 export default Search;
