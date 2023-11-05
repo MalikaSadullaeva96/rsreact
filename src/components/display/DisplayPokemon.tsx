d4b7266b66f67f8710090317305cde2588c1948a
Malika Sadullaeva <malika.sadullaeva@scandiweb.com>
2023-11-05 17:00:50 +0500

    feat: add display

--- src/components/display/DisplayPokemon.tsx
+++ src/components/display/DisplayPokemon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PokemonData } from "../type";
-import "./DisplayPokemon.css"
+import "./DisplayPokemon.css";
 
 interface DisplayPokemonProps {
   pokemon: PokemonData | null;
@@ -17,11 +17,11 @@ function DisplayPokemon({ pokemon }: DisplayPokemonProps) {
         <div>
           <h1>{pokemon.name}</h1>
           <img src={pokemon.img} alt={`Sprite of ${pokemon.name}`} />
-          <p>Species: {pokemon.species}</p>
-          <p>HP: {pokemon.hp}</p>
-          <p>Attack: {pokemon.attack}</p>
-          <p>Defense: {pokemon.defense}</p>
-          <p>Type: {pokemon.type}</p>
+          <h3>Species: {pokemon.species}</h3>
+          <h3>Type: {pokemon.type}</h3>
+          <h4>HP: {pokemon.hp}</h4>
+          <h4>Attack: {pokemon.attack}</h4>
+          <h4>Defense: {pokemon.defense}</h4>
         </div>
       ) : (
         <h1>No Pokemon selected</h1>
