fd4d1ecaa3ae923c259d88160ab1155ca651a741
Malika Sadullaeva <malika.sadullaeva@scandiweb.com>
2023-11-12 22:07:02 +0500

    fix:babel problem

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
